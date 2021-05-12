f558ca3aac1ccbdc01e53f421ab99ac12597f63a
adrien gonzalez <adrien@enzosystems.com>
2021-05-12 10:43:47 +0000

    Dev adrien feature add block

---
+++ public/js/jsfunctions.js
@@ -0,0 +1,43 @@
+ // "use strict";
+
+ function lockMasterKeyFunc()
+ {
+     let checkBox = document.getElementById("masterKey");
+     let inputControl = document.getElementById("idInputMasterKey");
+     let button = document.getElementById("idButtonGenerateKey");
+     
+     if (checkBox.checked == true){
+         inputControl.disabled = true;
+         button.disabled = true;
+     } else {
+         inputControl.disabled = false;
+         button.disabled = false;
+     }
+ }
+
+
+
+function generateModule(){
+    
+    
+    let formContainerElmnt = document.getElementsByClassName("formContainer")[0] ;
+    let prevElmnts = document.getElementsByClassName("propertyGroupContainer") ;
+
+    let lastId = prevElmnts.length - 2 ; // the 2 first el aren't modulegroups 
+
+    let lastModule = prevElmnts.namedItem( `moduleGroup${lastId}`) ;
+    console.log(lastModule)
+    let newElmnt = lastModule.cloneNode(true) ;
+    let newId = (lastId + 1)
+    newElmnt.id = `moduleGroup${newId}` ;
+
+    let inputs = newElmnt.getElementsByTagName('input');
+    for (el of inputs) {
+        let newString = el.id.replace(/\d+$/ , newId);
+        el.id = newString;
+    }
+
+    formContainerElmnt.insertBefore(newElmnt ,  formContainerElmnt.lastElementChild) 
+}
+
+document.getElementById("addNodeButton").addEventListener("click", generateModule);
