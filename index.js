a209257645018e5570e5f53e6ef2115d591bdf96
adrien gonzalez <adrien@enzosystems.com>
2022-02-04 14:47:50 +0100

    fix

--- index.js
+++ index.js
@@ -30,20 +30,25 @@ app.use(express.json()) ;// for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public')); //to serve the form located in public/index.html
 app.engine('ntl', (filePath, options, callback) => { // define a template engine to update the form submit to the correct host 
-  fs.readFile(filePath,  (err, content) => {
-    if (err) return callback(err)
+  fs.readFile(filePath, (err, content) => {
+    if (err) { 
+      winstonLogger.log('error', JSON.stringify(err));
+      return callback(err);
+    }
     // this is an extremely simple template engine
     let rendered = content.toString()
                     .replace('#scheme#', scheme)
                     .replace('#host#', host)
                     .replace('#port#', port)
     return callback(null, rendered);
-  })
+  });
 });
 app.set('views', './Views') // specify the views directory
 app.set('view engine', 'ntl') // register the template engine
 app.use(api);
 //start the app server on defined port 
 app.listen(port, host, () => {
+  
+    winstonLogger.log('info' , 'enzovault app is running at ' + scheme +  host + port);
     console.log('enzovault app is running at %s://%s:%s', scheme, host, port);
 });
