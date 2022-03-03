a3b3e86078b64db258c8b0bb46fd1153bcbcaff0
adrien gonzalez <adrien@enzosystems.com>
2022-03-03 12:55:33 +0000

    Update Routes/routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,6 +7,8 @@ api.post('/encrypt', encryptToEnzoVaultFile );
 api.post('/decrypt', decryptFromEnzoVaultFile);
 //endpoint to download the file
 api.get('/file/:name',  downloadEnzoVaultFile) ;
+//health
+api.get('/health', (req, res) => res.send("OK"));
 //endpoint serving the rendered form (with url from env) 
 api.get('/', function (req, res) {
     res.render('form', { scheme: process.env.SCHEME , host: process.env.HOST, port: process.env.PORT });
